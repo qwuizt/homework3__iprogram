22fbb2c6a52458c8fd8b3612b87365edcb190781
Qwuizt <boringfine@gmail.com>
2022-08-16 16:13:21 +0300

    задание 23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿void Cube( int N)
+{
+    Console.Write($"{N} --> ");
+    int i = 1;
+    while (i < N)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+        i++;
+    }
+    Console.WriteLine($"{Math.Pow(N,3)}.");
+}
+Cube(3);
+Cube(5);
