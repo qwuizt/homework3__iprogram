befc113074ce43ce7c6c181123c5258ea0f31be3
Qwuizt <boringfine@gmail.com>
2022-08-16 14:35:06 +0300

    задание 19

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿void Palindrom(int Digit)
+{
+    string StrDigit  = Digit.ToString();
+    if (StrDigit.Length == 5 && StrDigit[0]==StrDigit[4] && StrDigit[1]==StrDigit[3])
+    {
+        Console.WriteLine($"{Digit} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{Digit} не является палиндромом или не является пятизначным числом");
+    }  
+}
+
+Palindrom(Convert.ToInt32(Console.ReadLine()));
